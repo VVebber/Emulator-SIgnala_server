2ebea344a6487bb6cb3f5e755f14d9e07b1e86a2
vvebber <malihevdanil@yandex.ru>
2024-09-04 16:27:58 +0300

    RemuveFiles

--- build/Qt_6_7_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_Server_FILE /Users/malysevdanil/Documents/Projects/P2/Server/build/Qt_6_7_2_for_macOS-Debug/Server.app/Contents/MacOS/Server)
-set(__QT_DEPLOY_TARGET_Server_TYPE EXECUTABLE)
