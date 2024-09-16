2810dd721ebeba12473d433251f399285b0d2fec
Danil <malihevdanil@yandex.ru>
2024-09-16 17:11:36 +0300

    end2

--- src/main.cpp
+++ src/main.cpp
@@ -11,9 +11,34 @@ int main(int argc, char *argv[])
 
     Server* server = nullptr;
 
-    qDebug() << argv[0];
-    int Port = QString(argv[a.arguments().indexOf("-p") + 1]).toInt();
-    server = new Server(Port != 0? Port : 1024);
+    if (argc != 3)
+    {
+      return 1;
+    }
+
+    QStringList args = a.arguments();
+    int port = 1024;
+    int portArgIndex = args.indexOf("-p");
+    if(portArgIndex < 0 || portArgIndex == 2)
+    {
+      return 2;
+    }
+    else
+    {
+      QString portStr = args[portArgIndex + 1];
+      bool isOk = true;
+      int argPort = portStr.toInt(&isOk);
+
+      if (!isOk)
+      {
+        return 3;
+      }
+
+      port = argPort;
+    }
+
+
+    server = new Server(port);
     server->connection();
 
     return a.exec();
