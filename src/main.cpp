fde66fa9ad3341d7d4029ede3e9b432b14558680
Danil <malihevdanil@yandex.ru>
2024-09-16 17:07:43 +0300

    end2

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     qRegisterMetaType<qintptr>("qintptr");
-
     Manager::getInstance()->start();
 
     Server* server = nullptr;
-    int Port = QString(argv[a.arguments().indexOf("-p") + 1]).toInt();
 
+    qDebug() << argv[0];
+    int Port = QString(argv[a.arguments().indexOf("-p") + 1]).toInt();
     server = new Server(Port != 0? Port : 1024);
     server->connection();
 
