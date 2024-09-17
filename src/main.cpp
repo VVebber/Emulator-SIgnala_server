bed526638129dee36291ea042d4ee51d666a7aaf
vvebber <malihevdanil@yandex.ru>
2024-09-17 10:10:05 +0300

    end5

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,49 @@
-#include "mainwindow.h"
+#include "server.h"
 
-#include <QApplication>
+#include <QCoreApplication>
+
 
 int main(int argc, char *argv[])
 {
-  QApplication a(argc, argv);
-
+  QCoreApplication a(argc, argv);
   qRegisterMetaType<qintptr>("qintptr");
-  MainWindow w;
-  QObject::connect(&a, &QApplication::aboutToQuit, &w, &MainWindow::closeMainWindow);
+  Manager::getInstance().startThread();
 
-  w.show();
+  Server* server = nullptr;
+
+  if (argc != 3)
+  {
+
+    return 1;
+  }
+
+  QStringList args = a.arguments();
+  int port = 1024;
+  int portArgIndex = args.indexOf("-p");
+  if(portArgIndex < 0 || portArgIndex == 2)
+  {
+    return 2;
+  }
+  else
+  {
+    QString portStr = args[portArgIndex + 1];
+    bool isOk = true;
+    int argPort = portStr.toInt(&isOk);
+
+    if (!isOk)
+    {
+      return 3;
+    }
+
+    port = argPort;
+  }
+
+  server = new Server(port);
+  server->connection();
+
+  QObject::connect(&a, &QCoreApplication::aboutToQuit, server, &Server::close);
+  QObject::connect(&a, &QCoreApplication::aboutToQuit, server, &Server::deleteLater);
+  QObject::connect(&a, &QCoreApplication::aboutToQuit, &Manager::getInstance(), &Manager::deleteLater);
+
   return a.exec();
 }
